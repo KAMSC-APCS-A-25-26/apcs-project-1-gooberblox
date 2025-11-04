c655f651f5a5700da89cffb58afb3aab319c1d9c
gooberblox <bwrm2009@gmail.com>
2025-11-04 13:14:23 -0500

    done

--- src/main/java/gameState.java
+++ src/main/java/gameState.java
@@ -1,13 +1,11 @@
 public class gameState {
     // fartballs tracker
-    public User player;
     public int fightsWon = 0;
     public int fightsLost = 0;
     public int fightsIgnored = 0;
     public int zenScore = 0; // for #zen ending
     public boolean beatConner;
     public boolean beatJohnny;
-    public boolean beatJack;
     public boolean choseFighting;
     public boolean wonChampionship;
     public boolean choseZen;
